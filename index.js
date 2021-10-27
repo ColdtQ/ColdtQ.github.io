6695ffbe35d178cba81dcf81fb689c591b38f748
Trevor <trevor.romano2@Outlook.com>
2021-10-26 20:44:24 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const btn = document.getElementById("sendBtn");
+btn.addEventListener("click", sendBots);
+
+function sendBots() {
+  const code = document.getElementById("code")?.value;
+  const amount = document.getElementById("amount")?.value;
+  const botName = document.getElementById("name")?.value;
+
+  if (isNaN(code)) {
+    return alert(`The game code must be a number`);
+  } else if (isNaN(amount)) {
+    return alert(`The amount of bots must be a number`);
+  }
+
+  if (!code) {
+    return alert(`Please specify the game code.`);
+  } else if (!amount) {
+    return alert(`Please specify the amount of bots`);
+  } else if (!botName) {
+    return alert(`Please specify the bot name.`);
+  }
+
+  const body = { code, amount, name: botName };
+  const headers = {
+    "Content-Type": "application/json",
+  };
+
+  fetch("http://localhost:3000/send_bots", {
+    method: "POST",
+    headers,
+    body: JSON.stringify(body),
+  })
+    .then((res) => res.json())
+    .then((res) => console.log(res));
+}
---
+++ server/app.js
@@ -0,0 +1,57 @@
+let express = require("express");
+let Kahoot = require("kahoot.js-updated");
+let cors = require("cors");
+let app = express();
+
+const clientMap = new Map();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  cors({ methods: ["GET", "POST", "OPTIONS"], origin: "*", credentials: true })
+);
+
+app.listen(3000, () => {
+  console.log(`Listening on https://localhost:3000`);
+});
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.post("/send_bots", (req, res) => {
+  try {
+    let { code } = req.body;
+    let { amount } = req.body;
+    let { name } = req.body;
+
+    if (!code || !amount || !name)
+      return res
+        .json({
+          message: `Request received but didn't find the code, amount, or name`,
+        })
+        .status(401);
+
+    for (let i = 0; i < amount; i++) {
+      let client = new Kahoot();
+      clientMap.set(clientMap.size, client);
+      client.join(code, `${name}-${clientMap.size}`).catch(console.error);
+      client.on("QuestionStart", (question) => {
+        question.answer(Math.floor(Math.random() * 5)).catch(console.error);
+      });
+      client.on("QuizEnd", () => {
+        res.status(200).json({ message: "Quiz ended" });
+        clientMap.clear();
+      });
+    }
+
+    res.status(200).json({ message: "Completed", amount });
+  } catch (err) {
+    console.error(err);
+  }
+});
